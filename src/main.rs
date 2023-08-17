af20a205dbee1b9f7c98d0957e3506710b970b72
Nathaniel Graham <nathaniel.graham@protonmail.com>
2023-08-17 17:45:20 -0600

    Basic rendering

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,71 @@ use specs::{prelude::*};
 use std::cmp::{max, min};
 use specs_derive::Component;
 
-fn main() {
-    println!("Hello, world!");
+struct State {
+    ecs: World // a World is an ECS
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+#[derive(Component)] // saves Component imlementation with VecStorage
+struct Position {
+    x: u32,
+    y: u32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+
+    let mut gs = State {
+        ecs: World::new()
+    };
+
+    // register Position and Renderable in the World
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs
+        .create_entity()
+        .with(Position {x: 40, y: 25})
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position {x: i*7, y: 20})
+            .with(Renderable {
+                glyph: rltk::to_cp437('D'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
+
+    rltk::main_loop(context, gs)
 }
