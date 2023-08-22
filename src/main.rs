7d54f50f82c552a6cbd3c95a5b758caf73acd97c
Nathaniel Graham <[nathaniel.graham@protonmail.com, ngraham@redhat.com]>
2023-08-21 21:38:39 -0600

    room can be added

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,71 @@ struct State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        Map::draw(&self.ecs, ctx);
         player_input(self, ctx);
         let positions = self.ecs.read_storage::<Position>();
         let renders = self.ecs.read_storage::<Render>();
         for (pos, render) in (&positions, &renders).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum Tile {
+    Floor,
+    Stone,
+}
+
+struct Map {
+    width: usize,
+    height: usize,
+    depth: usize,
+    tiles: Vec<Tile>,
+}
+
+impl Map {
+    fn xy_idx(&self, x: i32, y: i32) -> usize {
+        (y as usize * self.width) + x as usize
+    }
+
+    fn idx_xy(&self, pos: usize) -> (i32, i32) {
+        ((pos / self.width) as i32, (pos % self.width) as i32)
+    }
+
+    fn apply_room_to_map(&mut self, room : &Rect) {
+        for y in room.y1 +1 ..= room.y2 {
+            for x in room.x1 + 1 ..= room.x2 {
+                let idx = self.xy_idx(x, y);
+                self.tiles[idx] = Tile::Floor;
+            }
+        }
+    }
+
+    fn simple_80x50() -> Self {
+        let mut map = Map {
+            width: 80,
+            height: 50,
+            depth: 1,
+            tiles: vec![Tile::Stone; 80*50],
+        };
+        let room = Rect { x1: 20, x2: 40, y1: 20, y2: 40};
+        map.apply_room_to_map(&room);
+        map
+    }
+
+    fn draw(ecs: &World, ctx: &mut BTerm) {
+        let map = ecs.fetch::<Map>();
+        for (pos, tile) in map.tiles.iter().enumerate() {
+            let (y, x) = map.idx_xy(pos);
+            match tile {
+                Tile::Floor => {
+                    ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), to_cp437('.'));
+                },
+                Tile::Stone => {
+                    ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), to_cp437('#'));
+                }
+            }
         }
     }
 }
@@ -78,15 +138,12 @@ fn main() -> BError {
     gs.ecs.register::<Render>();
     gs.ecs.register::<Player>();
 
+    let map = Map::simple_80x50();
+    gs.ecs.insert(map);
+
     gs.ecs.create_entity()
         .with(Position { x: 5, y: 5 })
         .with(Render { glyph: to_cp437('@'), fg: RGB::named(YELLOW), bg: RGB::named(BLACK)})
-        .with(Player {})
-        .build();
-
-    gs.ecs.create_entity()
-        .with(Position { x: 10, y: 10 })
-        .with(Render { glyph: to_cp437('@'), fg: RGB::named(RED), bg: RGB::named(BLACK)})
         .with(Player {})
         .build();
     main_loop(context, gs)
